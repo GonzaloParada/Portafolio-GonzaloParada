a49271c61475b16ac6fce9370652b10364ca5b58
Gonzalo Parada <gonzaparada96@gmail.com>
2023-07-26 17:21:28 -0300

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@assets": path.resolve(__dirname, "./public/assets"),
-    },
-  },
   plugins: [react()],
 });
